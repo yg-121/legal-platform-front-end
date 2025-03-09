92997ebd770bebea45f85acc3ac98344037220bf
YG <yg@yg.yg.com>
2025-03-09 14:59:25 +0300

    before-back-end

---
+++ app.tsx
@@ -0,0 +1,12 @@
+import AuthPage from "./auth-page"
+
+function App() {
+  return (
+    <div className="App">
+      <AuthPage />
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AuthPage from './pages/auth-page';
+import './index.css'; // Tailwind styles
+
+const App: React.FC = () => {
+  return (
+     <div >
+      <AuthPage />
+    </div>
+  );
+};
+
+export default App;
